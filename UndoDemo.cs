1cb28c2e8955acfb5f5fdf545db2226c4b527714
Eashiong <31652568+Eashiong@users.noreply.github.com>
2022-09-15 18:02:56 +0800

    Add files via upload

---
+++ UndoDemo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Undo;
+namespace Undo.Demo
+{
+
+    public class UndoDemo
+    {
+        //Create an ChangeStack to store changes
+        private const uint LIMIT = 5;
+        private readonly ChangeStack<string> changes = new ChangeStack<string>(LIMIT);
+
+        //按数据方式
+        private void Fun()
+        {
+            Person person = new Person("");
+
+            //Add new undo, redo commands using ChangeStack.add(). When a change is added, it calls the change's execute() method. Use Change() for simple inline changes.
+            changes.Add(new Change<string>(
+                oldValue: person.Name,
+                execute: () => person.Name = "Jane",
+                undo: (oldValue) => person.Name = oldValue
+                ));
+
+            //Undo a change with undo().
+            //print(person.firstName); // Jane
+            changes.Undo();
+            //print(person.firstName); // John
+
+            //Redo the change with redo().
+            changes.Redo();
+            //print(person.firstName); // Jane
+
+        }
+
+        //按方法方式
+        private readonly ChangeStack<Action> changes2 = new ChangeStack<Action>(LIMIT);
+        private void Fun2()
+        {
+            Team team = new Team();
+            Person person = new Person("Jane");
+
+            changes2.Add(new Change<Action>(
+                oldValue: ()=>
+                {
+                    team.Remove(person);
+                },
+                execute: () =>
+                {
+                    team.Add(person);
+                },
+                undo: (oldValue) => oldValue()
+                ));
+
+            //print(team.people.Count); // 1
+
+            //Undo a change with undo().
+            changes2.Undo();
+            //print(team.people.Count); // 0
+
+            //Redo the change with redo().
+            changes.Redo();
+            //print(team.people.Count); // 1
+
+        }
+
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person(string name) => this.Name = name;
+
+    }
+
+    public class Team
+    {
+        public readonly List<Person> people = new List<Person>();
+        public void Add(Person person)
+        {
+            people.Add(person);
+        }
+        public void Remove(Person person)
+        {
+            people.Remove(person);
+        }
+    }
+}
---
+++ UndoStack.cs
@@ -0,0 +1,133 @@
+﻿
+
+//参考自 https://pub.dev/packages/undo
+
+using System.Collections.Generic;
+using System;
+namespace Undo
+{
+    public class ChangeStack<T>
+    {
+        // Limit changes to store in the history
+        private uint limit;
+
+        /// <summary>
+        /// Changes to keep track of
+        /// </summary>
+        public ChangeStack(uint limit) => this.limit = limit;
+
+
+        private readonly LinkedList<List<Change<T>>> history = new LinkedList<List<Change<T>>>();
+        private readonly LinkedList<List<Change<T>>> redos = new LinkedList<List<Change<T>>>();
+
+        /// <summary>
+        /// Can redo the previous change
+        /// </summary>
+        public bool CanRedo => redos.Count > 0;
+
+        /// <summary>
+        /// Can undo the previous change
+        /// </summary>
+        public bool CanUndo => history.Count > 0;
+
+        /// <summary>
+        /// Add New Change and Clear Redo Stack
+        /// </summary>
+        public void Add(Change<T> change)
+        {
+            change.Execute();
+            history.AddLast(new List<Change<T>>() { change });
+            MoveForward();
+        }
+
+        private void MoveForward()
+        {
+            redos.Clear();
+            if (limit > 0 && history.Count > limit + 1)
+            {
+                 history.RemoveFirst();
+            }
+        }
+        /// <summary>
+        /// Add New Group of Changes and Clear Redo Stack
+        /// </summary>
+        public void AddGroup(List<Change<T>> changes)
+        {
+            ApplyChanges(changes);
+            history.AddLast(changes);
+            MoveForward();
+        }
+
+        private void ApplyChanges(List<Change<T>> changes)
+        {
+            foreach (var change in changes)
+            {
+                change.Execute();
+            }
+        }
+
+
+        /// <summary>
+        /// Clear Undo History
+        /// </summary> 
+        public void Clear()
+        {
+            history.Clear();
+            redos.Clear();
+        }
+
+        /// <summary>
+        /// Redo Previous Undo
+        /// </summary> 
+        public void Redo()
+        {
+            if (CanRedo)
+            {
+                var changes = redos.First.Value;
+                redos.RemoveFirst();
+                ApplyChanges(changes);
+                history.AddLast(changes);
+            }
+        }
+        /// <summary>
+        /// Undo Last Change
+        /// </summary> 
+        public void Undo()
+        {
+            if (CanUndo)
+            {
+                var changes = history.Last.Value;
+                history.RemoveLast();
+                foreach (var change in changes)
+                {
+                    change.Undo();
+                }
+                redos.AddFirst(changes);
+            }
+        }
+    }
+
+    public class Change<T>
+    {
+        private readonly T oldValue;
+        private readonly Action execute;
+        private readonly Action<T> undo;
+
+        public Change(T oldValue, Action execute, Action<T> undo)
+        {
+            this.oldValue = oldValue;
+            this.execute = execute;
+            this.undo = undo;
+        }
+
+        public void Execute()
+        {
+            execute?.Invoke();
+        }
+        public void Undo()
+        {
+            undo?.Invoke(oldValue);
+        }
+
+    }
+}
